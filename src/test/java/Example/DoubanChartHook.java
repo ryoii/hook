6fb2e17745bbaf22df5cc254a080be84aad35c13
darkcloth <darkcloth@126.com>
2019-10-10 14:52:59 +0800

    Achieve Hook, a declarative crawler

---
+++ src/test/java/Example/DoubanChartHook.java
@@ -0,0 +1,20 @@
+package Example;
+
+import core.crawler.Hook;
+import core.model.Links;
+
+public class DoubanChartHook {
+
+    public static void main(String[] args) {
+        Hook.newHook().restTime(5).retryTime(5)
+                .addSeed("https://movie.douban.com/chart", "chart")
+                .select("chart", "tr.item > td > a.nbg").type("movie")
+                .visitHandler(((page, tasks) -> {
+                    if (page.typeMatch("movie")) {
+                        String name = page.text("div[id=content]>h1>span[property=v:itemreviewed]");
+                        String rate = page.text("strong.rating_num");
+                        System.out.println(name + ": " + rate);
+                    }
+                })).start(5);
+    }
+}
